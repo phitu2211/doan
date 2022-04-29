2ec675459f8d2a5c6d37565f98619f11d004d0f1
phitu2211 <phitu.fiincredit@gmail.com>
2022-04-29 17:25:11 +0700

    init

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 # coding=utf-8
 try:
-  from tkinter import *
+    from tkinter import *
 except ImportError:
-  from Tkinter import *
+    from Tkinter import *
 
-win = Tk() 
-win.title("Mã hóa")
-win.resizable(False, False) 
 
-window_height = 500
-window_width = 900
+class App:
+    def __init__(self, parent):
+        btn = Button(
+            win,
+            text="Exit",
+            command=win.destroy,
+            background='#48483E',
+            foreground='#CFD0C2'
+        )
+        btn.place(x=80, y=100)
 
-btn = Button(
-    win,
-    text="Exit",
-    command=win.destroy,
-    background='#48483E',
-    foreground='#CFD0C2'
-)
-btn.place(x=80, y=100)
 
-screen_width = win.winfo_screenwidth()
-screen_height = win.winfo_screenheight()
+if __name__ == '__main__':
+    win = Tk()
+    win.resizable(False, False)
+    win.title("Mã hóa")
 
-x_cordinate = int((screen_width/2) - (window_width/2))
-y_cordinate = int((screen_height/2) - (window_height/2))
+    window_height = 500
+    window_width = 900
 
-# Hiển thị giữa màn hình
-win.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
-win.configure(bg='#272822')
-win.mainloop()
+    screen_width = win.winfo_screenwidth()
+    screen_height = win.winfo_screenheight()
+
+    x_cordinate = int((screen_width/2) - (window_width/2))
+    y_cordinate = int((screen_height/2) - (window_height/2))
+
+    # Hiển thị giữa màn hình
+    win.geometry("{}x{}+{}+{}".format(window_width,
+                 window_height, x_cordinate, y_cordinate))
+    App(win)
+    win.configure(background='white')
+    win.mainloop()
